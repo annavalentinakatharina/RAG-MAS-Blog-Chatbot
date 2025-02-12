52523358fb22763dca86fb000e794d34da86a8d3
hansla29 <avkh@gmx.at>
2025-02-12 18:05:33 +0100

    last code improvements

--- src/ba_ragmas_chatbot/logger_config.py
+++ src/ba_ragmas_chatbot/logger_config.py
@@ -5,7 +5,6 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
         logging.FileHandler("app.log"),
-        #logging.StreamHandler()
     ]
 )
 
